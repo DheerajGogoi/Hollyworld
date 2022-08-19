d35e78ffc7a9ea6e95ff5a6c352a70bf93e5c92e
DheerajGogoi <dheerajgogoi2@gmail.com>
2022-08-19 13:08:17 +0530

    fix: react-routing fixed

--- src/App.js
+++ src/App.js
@@ -11,18 +11,21 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import { BrowserRouter, Routes, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Movies from './Pages/Movies';
+import PageNotFound from './Pages/PageNotFound';
+import TvShows from './Pages/TvShows';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path='/home' exact component={Home} />
-        <Route path='*' exact>
-          <Redirect to='/home' />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/movies' element={<Movies />}/>
+        <Route path='/tvshows' element={<TvShows />}/>
+        <Route path='*' element={<PageNotFound />}/>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,66 @@
-import { Box, Text } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import {
+    Box,
+    Text,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+    IconButton,
+    Divider
+} from '@chakra-ui/react';
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log(location.pathname);
     return (
         <Box
             bg='#38B2AC'
             w='100%'
             p={4}
-            color='white'
             textAlign='center'
             className='header-box'
+            position='relative'
         >
-            <Text fontSize={36} fontWeight={500}>MOVIELUX</Text>
-            <Text fontStyle='italic'>Find Movies, TV shows and more</Text>
+            <Text fontSize={36} fontWeight={500} color='white'>MOVIELUX</Text>
+            <Text fontStyle='italic' color='white'>Find Movies, TV shows and more</Text>
+            <Box position='absolute' right='20px' top='20px'>
+                <Menu>
+                    <MenuButton
+                        as={IconButton}
+                        aria-label='Options'
+                        icon={<HamburgerIcon />}
+                        variant='outline'
+                        color='white'
+                        _hover={{ background: 'transparent' }}
+                    />
+                    <MenuList color='black'>
+                        <MenuItem fontWeight={location.pathname === '/' ? 'bold' : 'normal'} onClick={() => navigate('/')}>
+                            Home
+                        </MenuItem>
+                        <MenuItem fontWeight={location.pathname === '/movies' ? 'bold' : 'normal'} onClick={() => navigate('/movies')}>
+                            Movies
+                        </MenuItem>
+                        <MenuItem fontWeight={location.pathname === '/tvshows' ? 'bold' : 'normal'} onClick={() => navigate('/tvshows')}>
+                            TV Shows
+                        </MenuItem>
+                        <MenuItem fontWeight={location.pathname === '/toprated' ? 'bold' : 'normal'}>
+                            Top Rated
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem fontWeight={location.pathname === '/search' ? 'bold' : 'normal'}>
+                            Search
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </Box>
         </Box>
     )
 }
